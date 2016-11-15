ca7dc772cfdb962c20a73d9c3f53841cbf58451c
Matt Lai <matthewlai.110@gmail.com>
2016-11-14 16:26:19 -0800

    Final SQL insert files

---
+++ Documents/Database Materials/Table SQL Files/shipment (unShipped).sql
@@ -0,0 +1,6 @@
+
+insert into shipment (shipment_id, location_id, shipment_date, hasShipped) values (21, 4, '2016/11/11', 0);
+insert into shipment (shipment_id, location_id, shipment_date, hasShipped) values (22, 10, '2016/11/08', 0);
+insert into shipment (shipment_id, location_id, shipment_date, hasShipped) values (23, 19, '2016/11/23', 0);
+insert into shipment (shipment_id, location_id, shipment_date, hasShipped) values (24, 12, '2016/11/06', 0);
+insert into shipment (shipment_id, location_id, shipment_date, hasShipped) values (25, 1, '2016/11/28', 0);
