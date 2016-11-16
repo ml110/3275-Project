d229cab518a0bda7435009e84e276be8ba45a43e
Matt Lai <matthewlai.110@gmail.com>
2016-11-16 00:32:25 -0800

    Updated SQL file. Added error handling to Receiving Form.

--- Application/WarehouseManager/WarehouseManager/Receiving.cs
+++ Application/WarehouseManager/WarehouseManager/Receiving.cs
@@ -27,20 +27,27 @@ namespace WarehouseManager
         MySqlCommand command = new MySqlCommand();
 
         private int lookupID; //OrderID
+
+		//these for storing the values I need to insert
+		List<int> listSkews = new List<int>();
+		List<int> listAmounts = new List<int>();
         
+		//public Receiving(MySqlConnection conn, MySqlCommand cmd)
         public Receiving()
 		{
 			InitializeComponent();
-            DBConnect();
+            DBConnect(); //REMOVE THIS IN FINAL VERSION
 		}
 
-        //TEMP CONNECT METHOD; REMOVE ONCE ACTUAL DB IS UP
+        //TEMP CONNECT METHOD; REMOVE FROM FINAL VERSION
         private void DBConnect()
         {
-            string server = "localhost";
+			string server = "173.180.133.176";
+			//string server = "localhost";
             string db = "hi-tec";
             string id = "root";
-            string pass = "";
+			string pass = "superpassword";
+            //string pass = "superpassword";
             string port = "3306";
 
             string connectionString = "SERVER=" + server + ";PORT=" + port + ";DATABASE=" + db + ";UID=" + id + ";PASSWORD=" + pass + ";";
@@ -60,25 +67,29 @@ namespace WarehouseManager
             }
         }
 
-
+		//view the inputed order number
         private void btnLookup_Click(object sender, EventArgs e)
         {
-            try
-            {
-                lookupID = Int16.Parse(txtOrderID.Text);
-                displayOrder(lookupID);
-                updateOrderInfo(lookupID);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("ERROR: Are you sure you know what a number is? Because I don't think you do...", "HEY IDIOT", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+			try
+			{
+				lookupID = Int16.Parse(txtOrderID.Text);
+				displayOrder(lookupID);
+				//updateOrderInfo(lookupID);
+			}
+			catch (FormatException) //for shitty inputs
+			{
+				MessageBox.Show("Are you sure you know what a number is? Because I don't think you do...", "HEY IDIOT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			catch (ArgumentException ex) //for no rows/order
+			{
+				MessageBox.Show(ex.Message, "OOPS", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
         }
 
         //This method takes in a number (the orderID), and spits out the associated data to the DGV
         private void displayOrder(int oID)
         {
-            string QUERY = "SELECT P.product_name AS PRODUCTNAME, OP.quantity AS UNITAMOUNT, concat(\"$\", (round((OP.quantity * P.product_price), 2))) AS TOTALPRICE FROM `order` AS O";
+            string QUERY = "SELECT P.product_id AS SKU, P.product_name AS PRODUCTNAME, concat(OP.quantity, \" units\") AS AMOUNT, concat(\"$\", (round((OP.quantity * P.product_price), 2))) AS TOTALPRICE, OP.isDamaged AS DAMAGED FROM `order` AS O";
                 QUERY += " INNER JOIN order_product AS OP ON O.order_id = OP.order_id";
                 QUERY += " INNER JOIN product AS P ON OP.product_id = P.product_id";
                 QUERY += " INNER JOIN supplier AS S ON O.supplier_id = S.supplier_id";
@@ -92,6 +103,17 @@ namespace WarehouseManager
             MDA.Fill(ds, "ORDER");
 
             dgvLookup.DataSource = ds.Tables["ORDER"];
+
+			//Check for empty result meaning no record found
+			if (ds.Tables["ORDER"].Rows.Count == 0)
+			{
+				labOrderInfo.Text = "";
+				throw new System.ArgumentException("No order with the ID " + oID.ToString() + " was found.");
+			}
+			else
+			{
+				updateOrderInfo(oID);
+			}
         }
 
         //This method updates the left side panel with the order supplier and date
@@ -106,11 +128,26 @@ namespace WarehouseManager
 
             while (reader.Read())
             {
-                labOrderInfo.Text = "Order Supplier: " + reader["SNAME"].ToString() + "\n";
-                labOrderInfo.Text += "Order Date: " + reader["ODATE"].ToString();
+				string dateString = reader["ODATE"].ToString();
+				dateString = dateString.Substring(0, 10).Trim();
+				
+				labOrderInfo.Text = reader["SNAME"].ToString() + "\n";
+                labOrderInfo.Text += dateString;
             }
 
             reader.Close();
         }
+
+		//THIS needs to add the selected row from the top DGV into the bottom readonly DGV
+		private void btnAdd_Click(object sender, EventArgs e)
+		{
+			int skew = Int16.Parse(dgvLookup.SelectedRows[0].Cells[0].Value.ToString());
+			listSkews.Add(skew);
+			int amt = Int16.Parse(dgvLookup.SelectedRows[0].Cells[2].Value.ToString().Split(' ')[0]);
+			listAmounts.Add(amt);
+
+			//DataGridViewColumn DGVcol = dgvLookup.Columns.
+			//dgvChecklist.Rows.Add("test", "test", "test");
+		}
 	}
 }
