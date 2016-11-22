ce2237cbe42638c366a7d086cb6e2924744f758b
Curtis W <curtiswindsor90@gmail.com>
2016-11-22 00:59:39 -0800

    error and merge process

--- Application/WarehouseManager/WarehouseManager/Inventory.cs
+++ Application/WarehouseManager/WarehouseManager/Inventory.cs
@@ -114,5 +114,10 @@ namespace WarehouseManager
                 this.CloseConnection();
             }
         }
+
+        private void dgvInvDisplay_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
     }
